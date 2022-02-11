9d297dba611a663d4099b727df39618fe986bfc7
fadhlu <fadhlurahman.i@gmail.com>
2022-02-11 10:34:13 +0800

    fix words return

--- pages/api/words.ts
+++ pages/api/words.ts
@@ -6,14 +6,16 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const allWords = await fs.readFile(
-    path.join(__dirname, "../../../../.scripts/words.json"),
-    "utf-8"
-  );
+  const allWords = JSON.parse(
+    await fs.readFile(
+      path.join(__dirname, "../../../../.scripts/words.json"),
+      "utf-8"
+    )
+  ) as [{ katna: string; kata: string }];
 
   res.setHeader(
     "Cache-Control",
     "public, s-maxage=3600, stale-while-revalidate=86400"
   );
-  res.status(200).json(allWords.replace(/^\ufeff/g, ""));
+  res.status(200).json(allWords);
 }
